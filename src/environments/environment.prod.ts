81aa6e83c6d1e140bcf892478f818829ce74c2b1
Michael Stark <michael.stark@starkindustriesne.com>
2022-01-07 20:22:46 -0600

    Prod support.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://apps.starkindustriesne.com/danceagent-api',
+  clientId: '0oa3eu8m79HimDYhO5d7',
+  issuer: 'https://dev-00327502.okta.com/oauth2/default',
+  loginRedirectUri: 'https://apps.starkindustriesne.com/danceagent/callback',
+  logoutRedirectUri: 'https://apps.starkindustriesne.com/danceagent/'
 };
